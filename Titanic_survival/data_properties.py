ee44bbedad01f7c2f6de722b93c4e7121211b3a3
amilyxy <amilyxy@163.com>
2018-10-07 21:48:19 +0800

    rfr

--- Titanic_survival/data_properties.py
+++ Titanic_survival/data_properties.py
@@ -8,6 +8,7 @@ describe: Construct some figure to display the data properties
 """
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
 
 def Single_attributes(data):
     fig = plt.figure()
@@ -95,5 +96,36 @@ def Multiple_attributes(data):
     plt.ylabel("num")
     plt.title("Cabin and passengers")
     plt.show() 
-      
+
+'''
+通常遇到缺值的情况，我们会有几种常见的处理方式
+1.缺省样本比例极高，可舍弃，可能作为特征反而加入noise
+2.缺省样本比例适中，且非连续值特征属性，那就把NaN作为一个新类别，加到类别特征中
+3.如果缺值的样本适中，而该属性为连续值特征属性，有时候我们会考虑给定一个step(比如这里的age，我们可以考虑每隔2/3岁为一个步长)，然后把它离散化，之后把NaN作为一个type加到属性类目中。
+有些情况下，缺失的值个数并不是特别多，那我们也可以试着根据已有的值，拟合一下数据，补充上。
+'''
+#使用RandomForestClassifier 填补缺失年龄属性
+def set_missingAge(data):
+    #如我想查看Age为nan是不是Cabin一定为nan
+    #data_train.Cabin[pd.isnull(data_train.Age)].count()
+    # 把已知的数据类型特征取出丢进Random Forest Regressor
+    age_df = data[['Age', 'Fare', 'Parch', 'SibSp', 'Pclass']]
+    #乘客分为已知年龄和未知年龄两部分
+    know_age = age_df[age_df.Age.notnull()].as_matrix()
+    unknow_age = age_df[age_df.Age.istnull()].as_matrix()
+    # y - 目标年龄
+    y = know_age[:, 0]
+    # x - 特征属性值
+    X = know_age[:, 1:]
+    # fit到RandomForestRegressor之中
+    rfr = RandomForestRegressor(random_state = 0, n_estimators = 2000, n_jobs = -1)
+    rfr.fit(X, y)
+    # 用得到的模型进行未知年龄结果预测
+    predictedAge = rfr.predict(unknow_age[:, 1::])
+    # 用得到的预测结果填补原缺失数据
+    data.loc[(data.Age.isnull()), 'Age'] = predictedAges  
+    return data, rfr
+
+def set_Cabin_type(data):
+    
 
